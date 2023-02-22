c745b1c415aaf107ca2c6ecc801f9766cbed7a82
aleandre6579 <aleandre6579.simon@gmail.com>
2023-02-22 20:57:46 +0700

    Improving buttons

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -7,20 +7,43 @@ class Intro extends Component {
 
     } 
     render() { 
+        console.log(document.querySelectorAll(".magic"));
+        function circleBtnClicked(btnNum) {
+            console.log(btnNum);
+        }
+
+        const handleOnMouseMove = e => {
+            console.log("A");
+            const { currentTarget: target } = e;
+
+            const rect = target.getBoundingClientRect(),
+                x = e.clientX - rect.left,
+                y = e.clientY - rect.top;
+
+            target.style.setProperty("--mouse-x", '${x}px');
+            target.style.setProperty("--mouse-y", '${y}px');
+        }
+
+        for(const magic of document.querySelectorAll(".magic")) {
+            console.log("B");
+            magic.onmousemove = e => handleOnMouseMove(e);
+        }
+
         return (
             <div className="w3-display-container w3-center white-section">
                     <div className="title w3-container w3-padding-32 w3-hide-small">
                         <div className="name">Alexandre Simon</div>
                         <div className="software-dev">Software Developer</div>
                     </div>
+
                     <div className="btns">
-                        <div className="magic btn-container about-btn">
+                        <div onClick={() => circleBtnClicked(0)} className="magic btn-container about-btn">
                             <div className="circle-btn-txt">About</div>
                         </div>
-                        <div className="magic btn-container skills-btn">
+                        <div onClick={() => circleBtnClicked(1)} className="magic btn-container skills-btn">
                             <div className="circle-btn-txt">Skills</div>
                         </div>
-                        <div className="magic btn-container projects-btn">
+                        <div onClick={() => circleBtnClicked(2)} className="magic btn-container projects-btn">
                             <div className="circle-btn-txt">Projects</div>
                         </div>
                     </div>
