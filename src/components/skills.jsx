168b48cc48289b63a62d46eb6595dee76649df18
aleandre6579 <aleandre6579.simon@gmail.com>
2023-03-09 09:05:43 +0700

    Skills - Added images inside the containers, started on smooth position transition for the images when opening the containers

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -8,6 +8,28 @@ import '../css/w3.css';
 import backArrowWhite from '../imgs/backArrowWhite.png';
 import backArrowBlack from '../imgs/backArrowBlack.png';
 
+/* Container images */
+import c from '../imgs/c.png';
+import cSharp from '../imgs/cSharp.png';
+import css from '../imgs/css.png';
+import html from '../imgs/html.png';
+import java from '../imgs/java.png';
+import js from '../imgs/js.png';
+import python from '../imgs/python.png';
+
+import react from '../imgs/react.png';
+import unity from '../imgs/unity.png';
+
+import ambitious from '../imgs/ambitious.png';
+import creative from '../imgs/creative.png';
+import determined from '../imgs/determined.png';
+import solver from '../imgs/solver.png';
+import worker from '../imgs/worker.png';
+
+import team from '../imgs/team.png';
+import listener from '../imgs/listener.png';
+import communication from '../imgs/communication.png';
+
 class About extends Component {
     componentDidMount() {
         window.scrollTo(0,0);
@@ -111,21 +133,42 @@ class About extends Component {
                 <div className="containersRow">
                     <div className="containerSpace">
                         <div className="containerTitle">Languages</div>
-                        <div id='container1' onClick={() => openContainer('container1')} className="container box1"></div>
+                        <div id='container1' onClick={() => openContainer('container1')} className="container box1">
+                            <img style={{top: '30px', left: '80px'}} src={c} className='containerImg'/>
+                            <img style={{top: '80px', left: '200px'}} src={cSharp} className='containerImg'/>
+                            <img style={{top: '175px', left: '80px'}} src={css} className='containerImg'/>
+                            <img style={{top: '170px', left: '190px'}} src={html} className='containerImg'/>
+                            <img style={{top: '40px', left: '170px'}} src={java} className='containerImg'/>
+                            <img style={{top: '100px', left: '20px'}} src={js} className='containerImg'/>
+                            <img style={{top: '110px', left: '130px'}} src={python} className='containerImg'/>
+                        </div>
                     </div>
                     <div className="containerSpace">
                         <div className="containerTitle">Frameworks</div>
-                        <div id='container2' onClick={() => openContainer('container2')} className="container box2"></div>
+                        <div id='container2' onClick={() => openContainer('container2')} className="container box2">
+                            <img style={{top: '70px', left: '70px'}} src={react} className='containerImg'/>
+                            <img style={{top: '140px', left: '160px'}} src={unity} className='containerImg'/>
+                        </div>
                     </div>
                 </div>
                 <div className="containersRow">
                 <div className="containerSpace">
                         <div className="containerTitle">Social</div>
-                        <div id='container3' onClick={() => openContainer('container3')} className="container box3"></div>
+                        <div id='container3' onClick={() => openContainer('container3')} className="container box3">
+                            <img style={{top: '50px', left: '80px'}} src={team} className='containerImg'/>
+                            <img style={{top: '90px', left: '190px'}} src={listener} className='containerImg'/>
+                            <img style={{top: '160px', left: '100px'}} src={communication} className='containerImg'/>
+                        </div>
                     </div>
                     <div className="containerSpace">
                         <div className="containerTitle">Personal</div>
-                        <div id='container4' onClick={() => openContainer('container4')} className="container box4"></div>
+                        <div id='container4' onClick={() => openContainer('container4')} className="container box4">
+                            <img style={{top: '40px', left: '80px'}} src={ambitious} className='containerImg'/>
+                            <img style={{top: '60px', left: '180px'}} src={creative} className='containerImg'/>
+                            <img style={{top: '130px', left: '40px'}} src={determined} className='containerImg'/>
+                            <img style={{top: '190px', left: '110px'}} src={solver} className='containerImg'/>
+                            <img style={{top: '150px', left: '200px'}} src={worker} className='containerImg'/>
+                        </div>
                     </div>
                 </div>
 
